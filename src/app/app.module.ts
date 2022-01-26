2aa5ef2dd0f936f79ca20cce027f71afce508024
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2022-01-26 17:35:04 +0100

    Implemented ng-model from FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import loacalePl from '@angular/common/locales/pl';
 import { registerLocaleData } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 registerLocaleData(loacalePl);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
